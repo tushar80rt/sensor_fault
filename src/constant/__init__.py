9fbed5ffc0f79f4718b37cfab76436b5db0ba6aa
Tushar Singh <tushar80rt@gmail.com>
2026-05-14 23:31:24 +0530

    .gitignore

---
+++ src/constant/__init__.py
@@ -0,0 +1,13 @@
+import os
+
+
+AWS_S3_BUCKET_NAME ="water-fault"
+MONGO_DATABASE_NAME ="data"
+MONGO_COLLECTION_NAME ="waterfault"
+
+
+TARGET_COLUMNS = 'quality'
+MONGO_DB_URL = "mongodb+srv://tushar80rt_db_user:uz9PMN8byvn6yu7J@rag.7uyh4wq.mongodb.net/?appName=RAG"
+
+MODEL_FILE_NAME = "model"
+MODEL_FILE_EXTENSION = ".pkl"
